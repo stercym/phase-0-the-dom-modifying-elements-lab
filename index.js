00b79d7286b985dd780dd1b3966424c323dfa89f
Stercy Mutuku <mutukustercy93@gmail.com>
2025-06-29 00:35:24 +0300

    Adds and removes elements to the DOM

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+const main = document.getElementById("main");
+// Check if main exists then remove the <main> element with id="main"
+if (main) {
+  main.remove();
+}
+
+// Create a new headerer <h1> element
+const newHeader = document.createElement("h1");
+
+// Set its id to "victory"
+newHeader.id = "victory";
+
+// Set its text content to include my name
+newHeader.textContent = "Stercy Mutuku is the champion";
+
+// Append the newHeader to the body
+document.body.appendChild(newHeader);
